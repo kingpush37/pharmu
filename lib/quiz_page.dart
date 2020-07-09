8984be68ee4849fd2d171c3b21718ae7ce3fde42
unknown <troy.hilife@yahoo.com>
2020-07-08 19:07:01 -0700

    updated UI

--- lib/quiz_page.dart
+++ lib/quiz_page.dart
@@ -15,150 +15,166 @@ class _QuizPageState extends State<QuizPage> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.black,
-        body: Padding(
-          padding: const EdgeInsets.only(top: 20.0),
-          child: Column(
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  CustomCard(
-                    color: Colors.blue,
-                    customChild: LinearPercentIndicator(
-                      alignment: MainAxisAlignment.center,
-                      width: 385.0,
-                      lineHeight: 30.0,
-                      percent: 0.5,
-                      backgroundColor: Colors.white,
-                      linearGradient: LinearGradient(colors: [Color(0xFFCB218E), Color(0xFF6617CB)]),
-                    ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(9.0),
+              child: LinearPercentIndicator(
+                alignment: MainAxisAlignment.center,
+                width: 345.0,
+                lineHeight: 30.0,
+                percent: 0.5,
+                backgroundColor: Colors.white,
+                linearGradient: LinearGradient(colors: [Color(0xFFCB218E), Color(0xFF6617CB)]),
+                trailing: Padding(
+                  padding: const EdgeInsets.only(left: 5.0),
+                  child: Icon(
+                    Icons.local_hospital,
+                    color: Color(0xFFCB218E),
+                    size: 36.0,
                   ),
-                ],
+                ),
               ),
-              SizedBox(
-                height: 30.0,
-              ),
-              CustomCard(
-                color: Colors.black,
-                customChild: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            CustomCard(
+              color: Colors.black,
+              customChild: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
                   children: <Widget>[
                     Text(
-                        'Question 1/10',
+                        'Question 1',
                       style: TextStyle(
-                        fontSize: 20.0,
+                        fontSize: 25.0,
                         fontWeight: FontWeight.w900,
-                        color: Colors.white,
+                        color: Colors.white60,
                       ),
                     ),
-                  ],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                child: Divider(
-                  height: 2.0,
-                  thickness: 1.0,
-                  color: Colors.white,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Text(
-                        'What is the brand name for Tizanidine?',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25.0,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 100.0,
-              ),
-              Container(
-                padding: EdgeInsets.only(left: 20.0),
-                margin: EdgeInsets.symmetric(vertical: 10.0 , horizontal: 20.0),
-                height: 60.0,
-                decoration: BoxDecoration(
-                    border: Border.all(color: Color(0xFF1D1E33), width: 4.0, style: BorderStyle.solid),
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                child: Row(
-                  children: <Widget>[
+                    //TODO text baseline
                     Text(
-                        'Multiple Choice Answer',
+                      '/10',
                       style: TextStyle(
-                        color: Colors.white,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.white60,
+                        textBaseline: TextBaseline.alphabetic,
                       ),
                     ),
                   ],
                 ),
               ),
-              Container(
-                padding: EdgeInsets.only(left: 20.0),
-                margin: EdgeInsets.symmetric(vertical: 10.0 , horizontal: 20.0),
-                height: 60.0,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Color(0xFF1D1E33), width: 4.0, style: BorderStyle.solid),
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                child: Row(
-                  children: <Widget>[
-                    Text(
-                      'Multiple Choice Answer',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Divider(
+                endIndent: 12.0,
+                height: 2.0,
+                thickness: 1.0,
+                color: Color(0xFF6617CB),
               ),
-              Container(
-                padding: EdgeInsets.only(left: 20.0),
-                margin: EdgeInsets.symmetric(vertical: 10.0 , horizontal: 20.0),
-                height: 60.0,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Color(0xFF1D1E33), width: 4.0, style: BorderStyle.solid),
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                child: Row(
-                  children: <Widget>[
-                    Text(
-                      'Multiple Choice Answer',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 100.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Row(
                 children: <Widget>[
-                  FloatingActionButton.extended(
-                    onPressed: () {
-                      /**/
-                    },
-                    label: Text(
-                      'Next button',
+                  Expanded(
+                    child: Text(
+                      'What is the brand name for Tizanidine?',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25.0,
+                      ),
                     ),
-                    backgroundColor: Color(0xFF9B42F4),
                   ),
                 ],
               ),
-            ],
-          ),
+            ),
+            SizedBox(
+              height: 100.0,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 20.0),
+              margin: EdgeInsets.symmetric(vertical: 10.0 , horizontal: 20.0),
+              height: 60.0,
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white, width: 1.0, style: BorderStyle.solid),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                      'Multiple Choice Answer',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 20.0),
+              margin: EdgeInsets.symmetric(vertical: 10.0 , horizontal: 20.0),
+              height: 60.0,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.white, width: 1.0, style: BorderStyle.solid),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    'Multiple Choice Answer',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 20.0),
+              margin: EdgeInsets.symmetric(vertical: 10.0 , horizontal: 20.0),
+              height: 60.0,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.white, width: 1.0, style: BorderStyle.solid),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    'Multiple Choice Answer',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 100.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FloatingActionButton.extended(
+                  onPressed: () {
+                    /**/
+                  },
+                  label: Text(
+                    'Next',
+                  ),
+                  backgroundColor: Color(0xFF9B42F4),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
+
